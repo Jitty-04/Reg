8a037fb30b446462a888e6383dc79b534e7aca06
Jitty-04 <jittysjohn1997@gmail.com>
2022-11-22 11:58:17 +0530

    p

--- src/Resturant.java
+++ src/Resturant.java
@@ -1,2 +1,38 @@
 public class Resturant {
+    int tea;
+    int coffee;
+    int snacks;
+    int idli;
+    int dosa;
+    String name;
+    long phnno;
+    public Resturant(String name,long phnno){
+        this.name=name;
+        this.phnno=phnno;
+
+
+    }
+    int tea(int count)
+    {
+        this.tea=this.tea*count;
+        return this.tea;
+    }
+    int coffe(int count){
+        this.coffee=this.coffee*count;
+        return this.coffee;
+    }
+    int snacks(int count){
+        this.snacks=this.snacks*count;
+        return this.snacks;
+    }
+    int idli(int count){
+        this.idli=this.idli*count;
+        return this.snacks;
+    }
+    int dosa(int count){
+        this.dosa=this.dosa*count;
+        return this.dosa;
+    }
+
 }
+
